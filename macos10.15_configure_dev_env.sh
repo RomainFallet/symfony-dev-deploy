7f3de6fbc947cd298af2a9f4b8b8e621649cb04e
Romain Fallet <rfallet@numee.fr>
2020-03-12 17:25:55 +0100

    Update homebrew install instructions.

--- macos10.15_configure_dev_env.sh
+++ macos10.15_configure_dev_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 if [ ! $? = 0 ]; then
     exit 1
 fi
